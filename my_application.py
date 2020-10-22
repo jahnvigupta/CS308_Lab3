b7c36911d8d8f5ea567e65cba1cf48c06ad6a900
Aditya Malvi <adityamalvi2000@gmail.com>
2020-10-22 17:15:58 +0530

    Added refresh button & keyword file browse button with its calculation funtion

--- my_application.py
+++ my_application.py
@@ -26,10 +26,21 @@ class Application(Tk):
 
         self.file_label=Label(self, text = "File Not Chosen ." + self.file_location)
         self.file_label.pack()
+        self.keyword_label = Label(self, text = "Keywords File Not Chosen . " + self.keyword_location)
+        self.keyword_label.pack()
 
         #Code for making browse button for choosing file
         self.browse = tkinter.Button(self,text = "Text File",command = self.set_file_location)
         self.browse.pack()
+
+
+        #Code for making browse button for choosing keyword file
+        self.browse = tkinter.Button(self,text = "Keywords File",fg="brown", command = self.set_keyword_location)
+        self.browse.pack()
+
+        #Refresh button 
+        self.refresh = tkinter.Button(self, text="Refresh",fg="green",command=self.refresh_function)
+        self.refresh.pack()
 
         #Labels for statistics function
         self.number_of_words = Label(self, text="Number of Words: " + str(self.count_of_words))
@@ -42,6 +53,8 @@ class Application(Tk):
         self.most_frequent.pack()
         self.least_frequent = Label(self, text="Least Frequent Word ...." + (self.least_frequent_word))
         self.least_frequent.pack()
+        self.keyword_file = Label(self, text="Number of Sentences with keywords..." + str(self.count_in_keyword))
+        self.keyword_file.pack()
 
         #Quit button 
         self.quit = tkinter.Button(self, text="QUIT", fg="red",command=self.destroy)
@@ -52,6 +65,17 @@ class Application(Tk):
         self.file_location = filedialog.askopenfilename()
         self.file_label.config(text = "File: " + self.file_location)
         self.file_function()
+
+    def set_keyword_location(self):
+        self.keyword_location = filedialog.askopenfilename()
+        self.keyword_label.config(text = "File: " + self.keyword_location)
+        self.keyword_function()
+
+    def refresh_function(self):
+        if(self.file_location!=""):
+            self.file_function()
+        if(self.keyword_location!=""):
+            self.keyword_function()
 
     def file_function(self):
         data = open(self.file_location,"r")
@@ -88,6 +112,22 @@ class Application(Tk):
         self.least_frequent_word=min(map_words,key=map_words.get)
         self.least_frequent.config(text="Least Frequent Word : " + self.least_frequent_word)
 
+    def keyword_function(self):
+        data = open(self.file_location,"r")
+        raw_data=data.read()
+        keyword_data= open(self.keyword_location,"r")
+        keyword=keyword_data.read().split()
+        raw_data.replace("?",".")
+        raw_data.replace("!",".")
+        sentences=raw_data.split(".")
+        self.count_in_keyword=0
+        for i in range(len(sentences)):
+            check=0
+            for j in range(len(keyword)):
+                if(sentences[i].count(keyword[j])>0):
+                    check=1
+            self.count_in_keyword+=check
+        self.keyword_file.config(text="Number of Sentences with keywords: " + str(self.count_in_keyword))
 
 app = Application()
 app.title("Statistics For the File")
